7b73c256442b6c38fa005edf89026a2528b93e59
MaheshaDinu98 <maheeshadinushan@gmail.com>
2025-12-03 17:27:32 +0530

    hardcoded redirecting urls

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,8 @@ import {AuthProvider, } from "@asgardeo/auth-react";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider config={ {
-            signInRedirectURL: import.meta.env.VITE_SIGN_IN_REDIRECT_URL,
-            signOutRedirectURL: import.meta.env.VITE_SIGN_OUT_REDIRECT_URL,
+            signInRedirectURL: "https://88da18c0-a314-43c3-8556-74cf53339687.e1-us-east-azure.choreoapps.dev/",
+            signOutRedirectURL: "https://88da18c0-a314-43c3-8556-74cf53339687.e1-us-east-azure.choreoapps.dev/",
             clientID: "kTDc0kR6LffgJsfmm8SuLErXRe4a",
             baseUrl: "https://api.asgardeo.io/t/maheshadinushan",
             scope: [ "openid","profile" ]
