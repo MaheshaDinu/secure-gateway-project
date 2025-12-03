df2389907b45070002cf3d75e0849a75d2dcfa5c
MaheshaDinu98 <maheeshadinushan@gmail.com>
2025-12-03 16:19:48 +0530

    added VITE_ prefix to urls

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,8 @@ import {AuthProvider, } from "@asgardeo/auth-react";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider config={ {
-            signInRedirectURL: import.meta.env.SIGN_IN_REDIRECT_URL,
-            signOutRedirectURL: import.meta.env.SIGN_OUT_REDIRECT_URL,
+            signInRedirectURL: import.meta.env.VITE_SIGN_IN_REDIRECT_URL,
+            signOutRedirectURL: import.meta.env.VITE_SIGN_OUT_REDIRECT_URL,
             clientID: "kTDc0kR6LffgJsfmm8SuLErXRe4a",
             baseUrl: "https://api.asgardeo.io/t/maheshadinushan",
             scope: [ "openid","profile" ]
