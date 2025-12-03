160ba9c281c1b44e6aeda16ef85bc1ac33a8826c
MaheshaDinu98 <maheeshadinushan@gmail.com>
2025-12-03 18:31:25 +0530

    added a comment to trigger a build in choreo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {AuthProvider, } from "@asgardeo/auth-react";
+//added hardcoded redirect URLs 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
